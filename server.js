4dbbc4bcbe5b493bbcf218e2f0dff093451bc8aa
Diego Perez <m10fifa15@gmail.com>
2026-01-22 12:38:59 -0600

    HW1: create user feature + screenshots

--- server.js
+++ server.js
@@ -2,8 +2,17 @@ const express = require('express');
 const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 
+//crypto
+const crypto = require('crypto');
+
+function hashPassword(password) {
+  return crypto.createHash('sha256').update(password).digest('hex');
+}
+
 const app = express();
 const port = 3000;
+
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname));
@@ -15,29 +24,30 @@ const db = mysql.createConnection({
   database: 'userDB'
 });
 
-const crypto = require('crypto');
+//1st new code for homework 1 (changed with something originally given in lab 1)
 app.post('/login', (req, res) => {
-const username = req.body.username;
-const hashedPassword = crypto
-.createHash('sha256')
-.update(req.body.password)
-.digest('hex');
-const sql = `
-SELECT * FROM users
-WHERE username = ? AND password = ?
-`;
-db.query(sql, [username, req.body.password], (err, results) => {
-if (err) {
-console.error(err);
-return res.status(500).send('Server error');
-}
-if (results.length > 0) {
-res.send(`Welcome back, ${results[0].first_name}!`);
-} else {
-res.send('Invalid username or password.');
-}
+  const username = req.body.username;
+  const hashedPassword = hashPassword(req.body.password);
+
+  const sql = `
+    SELECT * FROM users
+    WHERE username = ? AND password = ?
+  `;
+
+  db.query(sql, [username, hashedPassword], (err, results) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Server error');
+    }
+
+    if (results.length > 0) {
+      res.send(`Welcome back, ${results[0].first_name}!`);
+    } else {
+      res.send('Invalid username or password.');
+    }
+  });
 });
-});
+
 
 
 app.get('/hello-user', (req, res) => {
@@ -57,6 +67,50 @@ app.get('/hello-user', (req, res) => {
     res.send(`Hello, ${user.first_name}!`);
   });
 });
+
+//second new code for homework 1
+
+app.post('/create-user', (req, res) => {
+  const { username, password, first_name, last_name, birthday } = req.body;
+
+  if (!username || !password || !first_name || !last_name || !birthday) {
+    return res.status(400).send('All fields are required.');
+  }
+
+  const hashedPassword = hashPassword(password);
+
+  //prevents duplicate usernames 
+  const checkSql = `SELECT id FROM users WHERE username = ? LIMIT 1`;
+  db.query(checkSql, [username], (checkErr, checkResults) => {
+    if (checkErr) {
+      console.error(checkErr);
+      return res.status(500).send('Server error');
+    }
+
+    if (checkResults.length > 0) {
+      return res.status(409).send('That username is already taken.');
+    }
+
+    const insertSql = `
+      INSERT INTO users (username, password, first_name, last_name, birthday)
+      VALUES (?, ?, ?, ?, ?)
+    `;
+
+    db.query(
+      insertSql,
+      [username, hashedPassword, first_name, last_name, birthday],
+      (err, result) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).send('Database insert error');
+        }
+
+        res.send(`User created successfully! New user id: ${result.insertId}`);
+      }
+    );
+  });
+});
+
 
 app.use((req, res) => {
   res.status(404).send('Not Found');
